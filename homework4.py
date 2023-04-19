a4464c2caddeb0eaafa53746f21667406b1b9afc
Pythonnoob13 <robomgeladze@yahoo.com>
2023-04-19 14:13:26 +0400

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -27,3 +27,45 @@ employee = {
 }
 employee.pop('name', 'salary')
 print(employee)
+
+
+
+
+
+
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = []
+final_toppings = []
+
+def get_toppings():
+    while True:
+        input_toppings = input(f'please choose toppings {available_toppings} and press Done: ').lower()
+
+        if input_toppings == 'done':
+            return requested_toppings
+        requested_toppings.append(input_toppings)
+get_toppings()
+
+def checker():
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            print(f"Adding {requested_topping}.")
+            final_toppings.append(requested_topping)
+        elif requested_topping not in available_toppings:
+            answer = input(f'we dont have {requested_topping} would u like a pizza without it? Yes/No - ').upper()
+            if answer == answer == 'NO':
+                print('we are sorry, see u next time!')
+                break
+            elif answer == 'YES':
+                continue
+
+checker()
+def final():
+
+    if len(final_toppings) > 0:
+        print(f'your pizza with {final_toppings} is done')
+    else:
+        print("Your pizza without topping is done")
+        quit()
+final()
